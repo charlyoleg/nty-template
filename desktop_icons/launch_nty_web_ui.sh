0d9d222a4c27007a71fc52ad1b1d69913d2ede99
charlyoleg <charlyoleg@fabfolk.com>
2021-07-05 22:55:52 +0200

    enhance launch script

--- desktop_icons/launch_nty_web_ui.sh
+++ desktop_icons/launch_nty_web_ui.sh
@@ -5,12 +5,12 @@ echo "Hello from launch_nty_web_ui.sh"
 cd $(dirname $0)
 pwd
 
-npm run
+#npm run
 npm run start_nty_web_ui
 
 
-echo "The Nty-web-ui has been stopped!"
-read -p "Press key to continue.. " -n1 -s
-echo "Bye from launch_nty_web_ui.sh"
+#echo "The Nty-web-ui has been stopped!"
+#read -p "Press key to continue.. " -n1 -s
+#echo "Bye from launch_nty_web_ui.sh"
 #sleep 2
 
