448baf3e3c72416b5de46025b5c74ab57c6384f0
charlyoleg <charlyoleg@fabfolk.com>
2021-07-06 15:54:46 +0200

    weird batch that stop on npm --version

--- desktop_icons/launch_nty_web_ui.bat
+++ desktop_icons/launch_nty_web_ui.bat
@@ -2,10 +2,12 @@
 ::
 echo "launch_nty_web_ui.bat is starting ..."
 node -v
-npm -v
+::npm -v
+::npm --version
 
 start http://localhost:8089
 npm run run_nty_web_ui
 ::npm run start_nty_web_ui
+
 pause
 echo "end of launch_nty_web_ui.bat"
