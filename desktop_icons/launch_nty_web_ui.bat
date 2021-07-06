428794d30511bcbcaaf3ea3eba9e4ed53f5af919
charlyoleg <charlyoleg@fabfolk.com>
2021-07-06 15:37:12 +0200

    hugly batch stuff

--- desktop_icons/launch_nty_web_ui.bat
+++ desktop_icons/launch_nty_web_ui.bat
@@ -2,6 +2,10 @@
 ::
 echo "launch_nty_web_ui.bat is starting ..."
 node -v
+npm -v
+
+start http://localhost:8089
 npm run run_nty_web_ui
+::npm run start_nty_web_ui
 pause
 echo "end of launch_nty_web_ui.bat"
